9e5ad632ab90166a0fd38dc598601c67ff135ba4
Zigao Wang <zigao.wang24@gmail.com>
2026-09-24 23:26:52 +0800

    feat: add 王子高 to home page for search

--- src/consts.ts
+++ src/consts.ts
@@ -13,6 +13,6 @@ export const AUTHOR = {
 };
 
 export const SITE_TITLE = "Zigao Wang";
-export const SITE_DESCRIPTION = "Student developer and maker in Shanghai. Building apps and hardware, playing tenor saxophone, shooting film, and running.";
+export const SITE_DESCRIPTION = "Zigao Wang (王子高), student developer and maker in Shanghai. Building apps and hardware, playing tenor saxophone, shooting film, and running.";
 export const BLOG_TITLE = "Zigao's Blog";
 export const BLOG_DESCRIPTION = "A high school developer writing about indie apps, books, talks, and whatever else is interesting.";
